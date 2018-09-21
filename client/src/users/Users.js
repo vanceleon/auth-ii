02c5fbb395027781fc575b368092336ac1f17823
vanceleon <vanceleon44@gmail.com>
2018-09-21 00:35:10 -0400

    building out the users and rendering each user

--- client/src/users/Users.js
+++ client/src/users/Users.js
@@ -1,16 +1,43 @@
 import React, { Component } from "react";
 // import { Route } from "react-router-dom";
-
+import axios from 'axios';
 
 class Users extends Component {
   state = {
+    users: [],
+  };
 
-};
 
-  
-    render() {
+  componentDidMount() {
+    const token = localStorage.getItem('jwt');
+    const reqOptions = {
+      headers: {
+        Authorization: token,
+      }
+    };
+
+    axios
+      .get('http://localhost:8000/api/users', reqOptions)
+      .then(res => {
+        console.log('Users Data: ', res.data);
+        // localStorage.setItem('jwt', res.data);
+        this.setState({users: res.data});
+      })
+      .catch(err => {
+        console.error('Axios Error: ', err);
+      });
+  }
+
+
+  render() {
     return (
-            <button>Logout</button>
+      <div>
+        <ul>
+          {this.state.users.map(user => {
+            return <li key={user.id}>{user.username}</li>;
+          })}
+        </ul>
+      </div>
     );
   }
 }
